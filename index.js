581866828d4b746ded6bd9b403bf078045d98420
Thomas James Barrasso <contact@tombarrasso.com>
2017-07-18 13:23:08 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './Game';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
